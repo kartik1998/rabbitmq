1d18e0f2d0710bb80080ecebf20255845a7bb7bc
Kartik <kartik@hyperverge.co>
2021-05-25 15:49:32 +0530

    Add: pub sub emitter and reciever code

---
+++ amqp.pub.sub/emit-log.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (err0, connection) {
+  if (err0) {
+    throw err0;
+  }
+  connection.createChannel(function (err, channel) {
+    if (err) {
+      throw err;
+    }
+    const exchange = 'logs';
+    const msg = process.argv.slice(2).join(' ') || 'Hello World!';
+
+    channel.assertExchange(exchange, 'fanout', {
+      durable: false,
+    });
+
+    channel.publish(exchange, '', Buffer.from(msg));
+    console.log(' [x] Sent %s', msg);
+  });
+
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
